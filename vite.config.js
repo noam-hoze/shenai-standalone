bf05520ad0fbf34aacedf7f5af0e28c8f0b2583f
noamhoze <noam.hoze@vayyar.com>
2025-07-03 12:50:31 -0400

    restore mkcert for mobile

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import mkcert from "vite-plugin-mkcert";
 // https://vite.dev/config/
 export default defineConfig({
     server: { https: false },
     plugins: [
         react(),
+        mkcert(),
         {
             name: "cross-origin-isolation",
             configureServer: (server) => {

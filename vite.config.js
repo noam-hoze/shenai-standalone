ec66ce02a4931bea3eeb765a5c0fcee5d194e25c
noamhoze <noam.hoze@vayyar.com>
2025-07-03 13:19:56 -0400

    Start on https

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 // https://vite.dev/config/
 export default defineConfig({
-    server: { https: false },
+    server: { https: true },
     plugins: [
         react(),
         mkcert(),
